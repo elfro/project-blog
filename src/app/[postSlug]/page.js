1ef566c3770029e63fb2dce4265606457dea5690
Ellina Frolova <frolova.ellina@gmail.com>
2024-02-07 18:32:03 +0200

    Load post data in details post page, add pages metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,39 @@
 import React from 'react';
 
 import BlogHero from '@/components/BlogHero';
+import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { loadBlogPost } from '@/helpers/file-helpers';
 
 import styles from './postSlug.module.css';
 
-function BlogPost() {
+export async function generateMetadata({params}) {
+  const {frontmatter: { title, abstract }} = await loadBlogPost(params.postSlug);
+
+  return {
+    title,
+    description: abstract,
+  }
+}
+async function BlogPost({params}) {
+  const postData = await loadBlogPost(params.postSlug);
+
+  const {frontmatter: { title, publishedOn}, content} = postData;
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={title}
+        publishedOn={publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote
+          source={content}
+          components={{
+            DivisionGroupsDemo
+          }}
+        />
       </div>
     </article>
   );
