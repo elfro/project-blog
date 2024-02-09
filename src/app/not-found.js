3f5fffbca10536f2c02c355afffa16f05bd921d1
Ellina Frolova <frolova.ellina@gmail.com>
2024-02-09 16:16:40 +0200

    Extract MDX components map, lazy load widget components by default

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { BLOG_TITLE } from '@/constants';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 
 function NotFound() {
